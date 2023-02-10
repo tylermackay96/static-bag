e4f2ca30a3e4084829b03e63216b7019aa340e01
Tyler <tyler.mackay@barclays.com>
2023-02-10 16:00:31 +0000

    Task completed - tests passed

--- Bag.js
+++ Bag.js
@@ -1,10 +1,16 @@
 class Bag {
+    static MAX_WEIGHT = 23;
+
     constructor(weight) {
-        if(!weight){
-            throw new Error('bag needs weight')
-        } 
-        this.weight = weight
+        if (!weight) {
+            throw new Error('Bag weight is required');
+        }
+        if (weight > Bag.MAX_WEIGHT) {
+            throw new Error(`Bag weight of ${weight}kg exceeds the maximum of ${Bag.MAX_WEIGHT}kg.`);
+        }
+        this.weight = weight;
     }
 }
 
-module.exports = Bag
+module.exports = Bag;
+
--- Bag.test.js
+++ Bag.test.js
@@ -1,13 +1,27 @@
-const Bag = require('./Bag')
+const Bag = require('./Bag');
 
 describe('Bag class', () => {
-    //bag has weight
-    test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
-    })
-    //if bag has no weight, return error
-    test('bag has no weight to return an error', () => {
-        expect(() => new Bag()).toThrowError('bag needs weight')
-    })
-})
+  //bag has weight
+  test('bag has weight', () => {
+    const bag = new Bag(23);
+    expect(bag.weight).toBe(23);
+  });
+
+  //if bag has no weight, return error
+  test('bag has no weight to return an error', () => {
+    expect(() => new Bag()).toThrowError('Bag weight is required');
+  });
+
+  describe('Bag', () => {
+    it('should create a bag with the correct weight', () => {
+      const bag = new Bag(20);
+      expect(bag.weight).toBe(20);
+    });
+
+    it('should throw an error if the weight exceeds the maximum', () => {
+      expect(() => {
+        new Bag(24);
+      }).toThrowError('Bag weight of 24kg exceeds the maximum of 23kg.');
+    });
+  });
+});
